b056b1e83e1d93d620b555e0fd4ef2156389cd4c
yukta <bajajyukta7@gmail.com>
2021-05-16 01:04:17 +0530

    Added home page, login and signup page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Medicine Exchange' });
 });
+
+/* Get Login page */
+router.get('/login', function(req, res) {
+    res.render('login.ejs')
+});
+
+/* Get Signup page*/ 
+router.get('/signup', function(req, res) {
+    res.render('signup.ejs') 
+});
+
+/* Get Profile Page */
+// router.get('/profile', isLoggedIn, function(req, res) {
+//     res.render('profile.ejs', {
+//         user : req.user
+//     });
+// });
+
+// // LOGOUT ==============================
+// router.get('/logout', function(req, res) {
+//     req.logout();
+//     res.redirect('/');
+// });
 
 module.exports = router;
