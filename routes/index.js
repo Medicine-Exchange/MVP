865d39a433c1a2e031bb2fc8f04a328025280632
yukta <bajajyukta7@gmail.com>
2021-05-16 18:31:56 +0530

    Bypass login page without Authentication

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
+
 // Define title of the website to be passed around pages
 const title = 'Medicine Exchange';
 
@@ -13,18 +14,29 @@ router.get('/', function (req, res, next) {
 router.get('/login', function (req, res) {
   res.render('login', { title })
 });
+// /* Process the login form */
+router.post('/login', function (req, res) {
+	//Please add next page i.e navigation bar here
+	res.render('signup', {title})
+});
 
 /* Get Signup page*/
 router.get('/signup', function (req, res) {
   res.render('signup', { title })
 });
 
+/* Get Signup page*/
+router.post('/signup', function (req, res) {
+	console.log("xyz");
+  //res.render('signup', { title })
+});
+
 /* Get Profile Page */
-// router.get('/profile', isLoggedIn, function(req, res) {
-//     res.render('profile.ejs', {
-//         user : req.user
-//     });
-// });
+router.get('/profile', function(req, res) {
+    res.render('profile.ejs', {
+        user : 'xyz'
+    });
+});
 
 // // LOGOUT ==============================
 // router.get('/logout', function(req, res) {
